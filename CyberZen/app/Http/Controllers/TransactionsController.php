0513cdf882b80ce512714d6c32dae207d4d16535
enzoveloso1996 <enzo.veloso888@gmail.com>
2020-03-18 00:13:03 +0800

    up

--- CyberZen/app/Http/Controllers/TransactionsController.php
+++ CyberZen/app/Http/Controllers/TransactionsController.php
@@ -25,6 +25,9 @@ class TransactionsController extends Controller
             'updated_by'            => $data['updated_by'],
         ]);
 
+        DB::table('tb_mf_carduser_records')->where('rfid_number', $data['rfid_number'])
+                                           ->update(['is_active'=> 1]);
+
         return redirect('cards/cms/teller/cardlist');
     }
 }
